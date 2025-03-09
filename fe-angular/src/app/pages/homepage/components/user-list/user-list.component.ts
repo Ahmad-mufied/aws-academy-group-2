cd9bd1924c8e9f0a24edf9f8bbed66fc5688106f
AkbarTambora <akbar.khaerullah.9@gmail.com>
2025-03-09 23:47:27 +0700

    FE-1: Add Raw List for User List, change node to 22 lts

--- fe-angular/src/app/pages/homepage/components/user-list/user-list.component.ts
+++ fe-angular/src/app/pages/homepage/components/user-list/user-list.component.ts
@@ -1,11 +1,69 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+
+
+export interface User {
+  name: string;
+  email: string;
+  birthDate: string;
+  joinedAt: string;
+  status: 'Active' | 'Inactive';
+  products: { basic: boolean; inter: boolean; adv: boolean };
+}
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  standalone: true,
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrls: ['./user-list.component.scss'],
+  imports: [
+    MatTableModule,
+    MatListModule,
+    CommonModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatIconModule
+  ],
 })
 export class UserListComponent {
+  displayedColumns: string[] = ['no', 'name', 'email', 'birthDate', 'joinedAt', 'status', 'product', 'actions'];
+  users: User[] = [
+    {
+      name: 'John Doe',
+      email: 'john@example.com',
+      birthDate: '1995-06-15',
+      joinedAt: '2024-02-10',
+      status: 'Active',
+      products: { basic: true, inter: true, adv: false }
+    },
+    {
+      name: 'Jane Smith',
+      email: 'jane@example.com',
+      birthDate: '1998-09-22',
+      joinedAt: '2023-11-01',
+      status: 'Inactive',
+      products: { basic: false, inter: true, adv: true }
+    }
+  ];
 
+  viewUser(user: User) {
+    console.log('Viewing user:', user);
+  }
+
+  assignProduct(user: User) {
+    console.log('Assigning product to user:', user);
+  }
+
+  editUser(user: User) {
+    console.log('Editing user:', user);
+  }
+
+  deleteUser(user: User) {
+    console.log('Deleting user:', user);
+  }
 }
