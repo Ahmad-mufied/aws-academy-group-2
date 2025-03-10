1d7f01111e0705c0618a2521018cf4bee422a391
DavidAfdal <davidafdal7@gmail.com>
2025-03-11 02:31:45 +0700

    feat (docs) : add example respone and request for swagger docs

---
+++ user-service/pkg/swagger/swagger_request.go
@@ -0,0 +1,17 @@
+package swagger
+
+type CreateUserRequest struct {
+	Name     string `json:"name" example:"David Afdal"`
+	Email    string `json:"email" example:"david.afdal@example.com"`
+	DoB      string `json:"date_of_birth" example:"2000-01-01"`
+	RoleID   string `json:"role_id" example:"550e8400-e29b-41d4-a716-446655440000"`
+	StatusID string `json:"status_id" example:"123e4567-e89b-12d3-a456-426614174000"`
+}
+
+type UpdateUserRequest struct {
+	Name     string `json:"name" example:"David Afdal"`
+	Email    string `json:"email" example:"david.afdal@example.com"`
+	DoB      string `json:"date_of_birth" example:"2000-01-01"`
+	RoleID   string `json:"role_id" example:"550e8400-e29b-41d4-a716-446655440000"`
+	StatusID string `json:"status_id" example:"123e4567-e89b-12d3-a456-426614174000"`
+}
